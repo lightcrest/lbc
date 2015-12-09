5172c8c6b48976dc35999acbb6c22ac18d390580
Alexander Guy <alexander.guy@lightcrest.com>
2015-12-09 10:56:39 -0800

    More rename.

--- interactive.bash
+++ interactive.bash
@@ -4,7 +4,7 @@ PATH=$LBCMDDIR:.
 
 for cmd in help cd pushd popd dirs pwd ; do enable -n $cmd ; done
 
-LBCMD="PATH=$_OLD_PATH $LBCMDDIR/lbcmd"
+LBCMD="PATH=$_OLD_PATH $LBCMDDIR/lbc"
 
 alias help="$LBCMD help"
 alias status="$LBCMD status"
